bhardwajs/gnucash

commit 9172d1dd235de31035e61d934959567024d865e8

Bug 799565 - Reconcile date default doesn't update to reflect…
previous reconciliation.

A one-word path error in xaccAccountClearReconcilePostpone.

libgnucash/engine/Account.cpp
@@ -4621,7 +4621,7 @@ xaccAccountSetReconcilePostponeBalance (Account *acc, gnc_numeric balance)
 void
 xaccAccountClearReconcilePostpone (Account *acc)
 {
-    set_kvp_gnc_numeric_path (acc, {KEY_RECONCILE_INFO, KEY_POSTPONE, "balance"}, {});
+    set_kvp_gnc_numeric_path (acc, {KEY_RECONCILE_INFO, KEY_POSTPONE}, {});
 }
 
 /********************************************************************\

commit 99506779e32c452bced0aa7652070982430f2479

Bug 799565 - Reconcile date default doesn't update to reflect previous reconciliation.
A one-word path error in xaccAccountClearReconcilePostpone.

The commit 9172d1dd23 accidentally reverted in 90662d72a2, reapply

libgnucash/engine/Account.cpp
@@ -4628,7 +4628,7 @@ xaccAccountSetReconcilePostponeBalance (Account *acc, gnc_numeric balance)
 void
 xaccAccountClearReconcilePostpone (Account *acc)
 {
-    set_kvp_gnc_numeric_path (acc, {KEY_RECONCILE_INFO, KEY_POSTPONE, "balance"}, {});
+    set_kvp_gnc_numeric_path (acc, {KEY_RECONCILE_INFO, KEY_POSTPONE}, {});
 }
 
 /********************************************************************\